03aa9565f9642e10d09077a8af32624722601005
Kevin Kim <kevinkim@Kevins-MacBook-Air.local>
2019-01-18 13:06:00 -0500

    the addition on how to get those movies!

--- js/main.js
+++ js/main.js
@@ -5,3 +5,21 @@ $(document).ready(() => {
       e.preventDefault();
     });
   });
+
+function getMovies(searchText){
+axios.get('http://www.omdbapi.com?s='+searchText)
+    .then((response) => {
+    console.log(response);
+    let movies = response.data.Search;
+    let output = '';
+    $.each(movies, (index, movie) => {
+        output += `
+        <div class="col-md-3">
+            <div class="well text-center">
+            <img src="${movie.Poster}">
+            <h5>${movie.Title}</h5>
+            <a onclick="movieSelected('${movie.imdbID}')" class="btn btn-primary" href="#">Movie Details</a>
+            </div>
+        </div>
+        `;
+    });
