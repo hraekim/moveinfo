53f458f679b7a60becddbc2fd55dfbfa75323680
Kevin Kim <kevinkim@Kevins-MacBook-Air.local>
2019-01-18 13:02:32 -0500

    documnet ready

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(() => {
+    $('#searchForm').on('submit', (e) => {
+      let searchText = $('#searchText').val();
+      getMovies(searchText);
+      e.preventDefault();
+    });
+  });
